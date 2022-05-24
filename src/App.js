24f539702a22be9aa28445b61bdf11e5f47db488
RobertoAlaniz30 <roberto.alaniz3002@gmail.com>
2022-05-24 08:39:25 -0500

    Adding some changes to the UI

--- src/App.js
+++ src/App.js
@@ -19,49 +19,55 @@ function App() {
 
   return (
     <div className="app">
-      <header className="search">
-        <input
-          value={location}
-          onChange={(event) => setLocation(event.target.value)}
-          onKeyPress={searchLocation}
-          placeholder="Enter Location"
-          type="text"
-        />
-      </header>
-      <main className="container">
-        <section className="top">
-          <div className="location">
-            <p>{data.name}</p>
+      <main className="main__container">
+        <div className="container">
+          <div className="search">
+            <input
+              value={location}
+              onChange={(event) => setLocation(event.target.value)}
+              onKeyPress={searchLocation}
+              placeholder="Enter Location"
+              type="text"
+            />
           </div>
-          <div className="temp">
-            {data.main ? <h1>{data.main.temp.toFixed()}°F</h1> : null}
-          </div>
-          <div className="description">
-            {data.weather ? <p>{data.weather[0].main}</p> : null}
-          </div>
-        </section>
-        <FontAwesomeIcon
-          icon={faCloud}
-          style={{ height: "130px", width: "70%", backgroundColor: "none" }}
-        />
-        <section className="bottom">
-          <div className="feels">
-            {data.main ? (
-              <p className="bold">{data.main.feels_like.toFixed()}°F</p>
-            ) : null}
-            <p>Feels Like</p>
-          </div>
-          <div className="humidity">
-            {data.main ? <p className="bold">{data.main.humidity}%</p> : null}
-            <p>Humidity</p>
-          </div>
-          <div className="wind">
-            {data.wind ? (
-              <p className="bold">{data.wind.speed.toFixed()} MPH</p>
-            ) : null}
-            <p>Wind Speed</p>
-          </div>
-        </section>
+          <section className="primary__section">
+            <section className="top">
+              <div className="location">
+                <p>{data.name}</p>
+              </div>
+              <div className="temp">
+                {data.main ? <h1>{data.main.temp.toFixed()}°F</h1> : null}
+              </div>
+              <div className="description">
+                {data.weather ? <p>{data.weather[0].main}</p> : null}
+              </div>
+            </section>
+            <FontAwesomeIcon
+              icon={faCloud}
+              style={{ height: "130px", width: "70%", backgroundColor: "none" }}
+            />
+            <section className="bottom">
+              <div className="feels">
+                {data.main ? (
+                  <p className="bold">{data.main.feels_like.toFixed()}°F</p>
+                ) : null}
+                <p>Feels Like</p>
+              </div>
+              <div className="humidity">
+                {data.main ? (
+                  <p className="bold">{data.main.humidity}%</p>
+                ) : null}
+                <p>Humidity</p>
+              </div>
+              <div className="wind">
+                {data.wind ? (
+                  <p className="bold">{data.wind.speed.toFixed()} MPH</p>
+                ) : null}
+                <p>Wind Speed</p>
+              </div>
+            </section>
+          </section>
+        </div>
       </main>
     </div>
   );
